8e4100fd3a523ee363e0dc1e311fa36000417637
mehdinoori20 <mehdi.noori1994@hotmail.com>
2025-02-17 12:17:51 +0100

    move our api key

--- components/Firebase.ts
+++ components/Firebase.ts
@@ -1,23 +1,10 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getAnalytics } from 'firebase/analytics';
-import { getAuth, onAuthStateChanged } from 'firebase/auth';
-
-
-const firebaseConfig = {
-
-    apiKey: "AIzaSyDXfN-1Al-Qg9Se8GnSpPT9cxpXStoXtxI",
-    authDomain: "new-backpack.firebaseapp.com",
-    //databaseURL: "https://DATABASE_NAME.firebaseio.com",
-    projectId: "new-backpack",
-    storageBucket: "new-backpack.firebasestorage.app",
-    messagingSenderId: "1030464807041",
-    appId: "1:1030464807041:android:ca9690217ded349c65ed80",
-
-};
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore/lite"
+import { getAuth } from "firebase/auth"
+import { firebaseConfig } from "../firebaseConfig"
 
 const firebaseApp = initializeApp(firebaseConfig)
 const auth = getAuth(firebaseApp)
-const db = getFirestore(firebaseApp);
+const db = getFirestore(firebaseApp)
 
-export {auth, db}
+export { auth, db }
